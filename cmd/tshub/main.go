de08df42891ccd5660e2b91a7a404f6d0cec56e2
christopher <post@c-ha.de>
2019-03-05 09:49:05 +0100

    add args options

--- cmd/tshub/main.go
+++ cmd/tshub/main.go
@@ -1,25 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/cha87de/tshub/commport"
 	"github.com/cha87de/tshub/datahub"
 	"github.com/cha87de/tshub/restapi"
 	"github.com/cha87de/tshub/restapi/operations"
 	"github.com/go-openapi/loads"
+	flags "github.com/jessevdk/go-flags"
 )
+
+var options struct {
+	APIPort  int    `long:"apiport" default:"8080"`
+	CommType string `long:"comm.type" default:"tcp"`
+	CommAddr string `long:"comm.addr" default:"127.0.0.1:12345"`
+}
 
 func main() {
 	// CONFIGS
-	// TODO: read from Flags
-	apiPort := 8080
-	commType := "tcp"
-	commAddr := "127.0.0.1:12345"
+	initializeFlags()
 
 	// first: main tshub features
 	dh := datahub.NewHub()
-	commportServer := commport.NewServer(commType, commAddr, dh)
+	commportServer := commport.NewServer(options.CommType, options.CommAddr, dh)
 	go commportServer.Start()
 
 	// second: swagger api
@@ -32,8 +38,31 @@ func main() {
 	server := restapi.NewServer(api)
 	server.ConfigureAPI()
 	defer server.Shutdown()
-	server.Port = apiPort
+	server.Port = options.APIPort
 	if err := server.Serve(); err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func initializeFlags() {
+	// initialize parser for flags
+	parser := flags.NewParser(&options, flags.Default)
+	parser.ShortDescription = "tshub"
+	parser.LongDescription = "Endpoint for kvmtop and kvmprofiler"
+	parser.ArgsRequired = false
+
+	// Parse parameters
+	if _, err := parser.Parse(); err != nil {
+		code := 1
+		if fe, ok := err.(*flags.Error); ok {
+			if fe.Type == flags.ErrHelp {
+				code = 0
+			}
+		}
+		if code != 0 {
+			fmt.Printf("Error parsing flags: %s", err)
+		}
+		os.Exit(code)
+	}
+
 }
